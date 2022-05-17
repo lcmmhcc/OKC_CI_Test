365d81b51c5591ada58a354653eb3ddd89e5378e
worm <lcmmhcc@126.com>
2022-05-17 15:21:47 +0800

    ecit

--- infura_rpc_test/setup.sh
+++ infura_rpc_test/setup.sh
@@ -21,14 +21,14 @@
 export infura_url="http://127.0.0.1:26659"
 export rpc_url="http://127.0.0.1:8545"
 
-export fromBlockNum=$(curl -X POST $rpc_url -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' | jq '.result')
-export fromBlockHash=$(curl -X POST $rpc_url -H 'Content-Type: application/json' -d "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[$fromBlockNum,false],\"id\":1}" | jq '.result|.hash')
+export fromBlockNum=$(curl -X POST $rpc_url -s -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' | jq '.result')
+export fromBlockHash=$(curl -X POST $rpc_url -s -H 'Content-Type: application/json' -d "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[$fromBlockNum,false],\"id\":1}" | jq '.result|.hash')
 
 #send tx
 #get tx
 #get block
 
 sleep 5
-export toBlockNum=$(curl -X POST $rpc_url -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' | jq '.result')
-export toBlockHash=$(curl -X POST $rpc_url -H 'Content-Type: application/json' -d "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[$toBlockNum,false],\"id\":1}" | jq '.result|.hash')
+export toBlockNum=$(curl -X POST $rpc_url -s -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' | jq '.result')
+export toBlockHash=$(curl -X POST $rpc_url -s -H 'Content-Type: application/json' -d "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[$toBlockNum,false],\"id\":1}" | jq '.result|.hash')
 
--- infura_rpc_test/test/test_eth_getCode.sh
+++ infura_rpc_test/test/test_eth_getCode.sh
@@ -35,6 +35,6 @@ test_eth_getCode_byBlockNum(){
 
 test_eth_getCode_byBlockHash(){
     #eth_getLogs
-    data="{\"jsonrpc\":\"2.0\",\"method\":\"eth_getCode\",\"params\":[\"0x45dD91b0289E60D89Cec94dF0Aac3a2f539c514a\", \"0x9872a49f5d49e9c2b938a33015250a2059ce9d150243bca74a1f0b884222c162"\],\"id\":1}" 
+    data="{\"jsonrpc\":\"2.0\",\"method\":\"eth_getCode\",\"params\":[\"0x45dD91b0289E60D89Cec94dF0Aac3a2f539c514a\", \"0x9872a49f5d49e9c2b938a33015250a2059ce9d150243bca74a1f0b884222c162\"],\"id\":1}" 
     run_test_Code $data
 }
